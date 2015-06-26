7da30b1e1d85356ff939727ec86c3f192fae9a48
Abby Yeh <radleaf@gmail.com>
2015-06-26 14:00:59 +0800

    first commit

---
+++ Group ProjectTests/Group_ProjectTests.swift
@@ -0,0 +1,36 @@
+//
+//  Group_ProjectTests.swift
+//  Group ProjectTests
+//
+//  Created by Shenju Huang on 2015/6/17.
+//  Copyright (c) 2015年 Shenju Huang. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Group_ProjectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
